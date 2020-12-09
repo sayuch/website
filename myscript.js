90583fb2b5144b3a914a588d83d2428116622aa3
sayu ch <73770776+sayuch@users.noreply.github.com>
2020-12-09 20:22:41 +0900

    Initial Commit
    ホームページを作成しました。

---
+++ myscript.js
@@ -0,0 +1,29 @@
+function play_se(){
+    var BGM = new Audio(`OMFG-Hello.mp3`);
+    BGM.play();
+}
+
+
+$(function(){
+    
+
+    history.pushState(null, null, null);
+    $(window).on("popstate", function(e){
+      if (!e.originalEvent.state){
+        history.pushState(null, null, null);
+        return;
+      }
+    });
+
+
+    $(`.modal`).modal();
+    $(`#alert`).modal(`open`);
+    $(`#close`).click(function(){
+        $(`#alert`).modal(`close`);
+        play_se();
+    });
+
+
+    var device = navigator.userAgent;
+    $(`#device`).text(device);
+});
